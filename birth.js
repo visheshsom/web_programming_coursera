89a1965e00a3e5d552a8277abd88e40f41485d46
visheshsom <visheshsom@gmail.com>
2017-03-11 15:55:43 +0530

    Update birth.js

--- birth.js
+++ birth.js
@@ -3,7 +3,7 @@ function birthday (event)
 
 	var dt = new Date(document.getElementById("date1").value);
 	
-	var month = dt.getMonth(document.getElementById("date1").value -1);
+	var month = dt.getMonth(document.getElementById("date1").value);
 	var date  = dt.getDate(document.getElementById("date1").value );
 	
 	var month1 = parseInt(month);
@@ -12,48 +12,25 @@ function birthday (event)
 	var birthday = new Date();
 	var cd = new Date();
 
-	cd.setDate(09,22,1997);
+		if (birthday.setDate(date1)&&birthday.setMonth(month1) > cd.setDate(09,22,1997))
+		{
+			birthday = birthday.getTime();
+			cd = cd.getTime();
 
-	birthday.setDate(date1);
-	birthday.setMonth(month1);
-
-
-
-	if (birthday.setDate(date1)&&birthday.setMonth(month1) < cd.setDate(09,22,1997))
-    {
-	    var theDate = cd - birthday ;
+			var theDate = birthday - cd ;
+			
+			theDate = (((theDate / 1000) / 60) / 60) / 24;
+			
+			var message = "<h4> " + theDate + " : Days Left</h4>";
+			
+			 document
+		    .getElementById("date_1")
+		    .innerHTML = message;
+		}
+	
 		
-		theDate = (((theDate / 1000) / 60) / 60) / 24;
-		
-		var message = "<h4>Your Birthday Is Before : " + theDate + " days</h4>";
-		document
-    	.getElementById("date_2")
-    	.innerHTML = message;
-	}
-
 	
 
-	if (birthday.setDate(date1)&&birthday.setMonth(month1) > cd.setDate(09,22,1997))
-	{
-		var theDate = birthday - cd ;
-		
-		theDate = (((theDate / 1000) / 60) / 60) / 24;
-		
-		var message = "<h4>" + theDate + " Days left</h4>";
-
-
-		document
-	    .getElementById("date_1")
-	    .innerHTML = message;
-	}
-
-	birthday = birthday.getTime();
-	cd = cd.getTime();
-
-	
-
-   
-	
 }
 
 	   
