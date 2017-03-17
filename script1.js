fba02a30b05d298cc8d6e0e75e82e15b81e3f66f
visheshsom <visheshsom@gmail.com>
2017-03-17 10:42:36 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded",
         
         // Call server to get the name
         $ajaxUtils
-          .sendGetRequest("data/name1.json", 
+          .sendGetRequest("name1.json", 
             function (res) {
               var message = 
                 res.firstName + " " + res.lastName
