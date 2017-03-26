2975acea67074279bfd16ce59ec75a616037efb0
visheshsom <visheshsom@gmail.com>
2017-03-26 12:45:16 +0530

    Update JS_1.js

--- JS_1.js
+++ JS_1.js
@@ -11,21 +11,21 @@ document.addEventListener("DOMContentLoaded",
             function (res) 
             {
               
-              var name = 
-              document.getElementById("s_name").value;
+               var name = 
+               document.getElementById("s_name").value;
               
               
-	       var jName = res.firstname;
+			   var jName = res.firstname;
                var jName1 = res.firstname1;
 			    
-	       var attenT1=23;
-               var attenL1=17;
+			   var attenT1=15;
+               var attenL1=13;
                var student_total1=attenL1+attenT1;
                var total_atten1=Number(res.Attendance_Theory1+res.AttendanceLecture1);
                var final_total1=student_total1/total_atten1*100;
 				
 			
-	       var  attenT1_s=23;
+			   var  attenT1_s=23;
                var  attenL1_s=20;
                var  student_total1_s=attenL1_s+attenT1_s;
                var  total_atten1_s=Number(res.Attendance_Theory2+res.AttendanceLecture2);
@@ -34,38 +34,36 @@ document.addEventListener("DOMContentLoaded",
 				
                 if (jName === name && final_total1<80) 
                     {
-			
-			 	document.querySelector('#content1')
-				.innerHTML = "<h4>"+jName+" : You've less Attendance in : " +res.Courses+ "</h4>";
-				document.querySelector('#content')
-				.innerHTML = "<h4>Attendence = " +final_total1+ "%</h4>";
-		    }
-
-		else if (jName1 === name && final_total1_s<80)
-		  {
-			
-			     document.querySelector('#content1')
-			     .innerHTML = "<h4>"+jName1+" : You've less Attendance in : " +res.Courses1+ "</h4>";
-			     document.querySelector('#content')
-			     .innerHTML = "<h4>Attendence = " +final_total1_s+ "%</h4>";
-			
 					
-		}
+						document.querySelector('#content1')
+						.innerHTML = "<h4>"+jName+" : You've less Attendance in : " +res.Courses+ "</h4>";
+						document.querySelector('#content')
+						.innerHTML = "<h4>Attendence = " +final_total1+ "%</h4>";
+						
+					}
+			
+			    else if (jName1 === name && final_total1_s<80)
+				{
+				
+						document.querySelector('#content1')
+						.innerHTML = "<h4>"+jName1+" : You've less Attendance in : " +res.Courses1+ "</h4>";
+						document.querySelector('#content')
+						.innerHTML = "<h4>Attendence = " +final_total1_s+ "%</h4>";
+					
+				}
+				
                 else
                     {
                       document
                       .getElementById("content1")
                       .innerHTML = "You're not in Defaulters....";
-		      document
+					  document
                       .getElementById("content")
                       .innerHTML = "";
  
                     }
                 
-              
-
-
-          });
+           });
       });
   }
 );
