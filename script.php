9e5f1e5ea1f9ff826b606782595a8abbe746f9bb
visheshsom <visheshsom@gmail.com>
2017-03-31 11:44:38 +0530

    Add files via upload

---
+++ script.php
@@ -0,0 +1,24 @@
+﻿<?php
+	$firstname = $_POST["textnames"];
+	echo $firstname."<br>";
+	$lasttname = $_POST["lastname"];
+	echo $lastname."<br>";
+	$gender    = $_POST["gen"];
+	echo $gender."<br>";
+	$dob = $_POST["dob"];
+	echo $dob."<br>";
+	$email_id = $_POST["emailid"];
+	echo $email_id."<br>";
+	$country = $_POST["Country"];
+	echo $country."<br>";
+	$state = $_POST["State"];
+	echo $state."<br>";
+	$city = $_POST["City"];
+	echo $city."<br>";
+	$pincode =$_POST["pincode"];
+	echo $pincode."<br>";
+	$address =$_POST["address1"];
+	echo $address."<br>";
+	$mobile_no =$_POST["mb-no"];
+	echo $mobile_no."<br>";
+?>
